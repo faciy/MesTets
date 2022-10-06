19b2c24956001a75c9ae93b56aa086ad370a1640
Yao Kouassi Charles Oth-Niel <charles@MacBook-Pro-de-Yao.local>
2022-10-06 13:53:00 +0000

    socket

--- App.tsx
+++ App.tsx
@@ -1,114 +1,109 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
-import React from 'react';
+import * as React from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
   Text,
-  useColorScheme,
   View,
+  StyleSheet,
+  TextInput,
+  Button,
+  ScrollView,
 } from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section: React.FC<{
-  title: string;
-}> = ({children, title}) => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+import ReconnectingWebSocket from 'react-native-reconnecting-websocket';
 
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+  const [serverState, setServerState] = React.useState('Loading...');
+  const [messageText, setMessageText] = React.useState('');
+  const [disableButton, setDisableButton] = React.useState(true);
+  const [inputFieldEmpty, setInputFieldEmpty] = React.useState(true);
+  const [serverMessages, setServerMessages] = React.useState([]);
+  var ws = React.useRef(
+    new ReconnectingWebSocket('ws://w567l.sse.codesandbox.io/', null, {
+      reconnectInterval: 1000,
+    }),
+  ).current;
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  React.useEffect(() => {
+    const serverMessagesList = [] as any;
+    ws.onopen = () => {
+      setServerState('Connected to the server');
+      setDisableButton(false);
+    };
+    ws.onclose = e => {
+      console.log('R', e);
+
+      setServerState('Disconnected. Check internet or server.');
+      setDisableButton(true);
+    };
+    ws.onerror = e => {
+      setServerState(e.message);
+    };
+    ws.onmessage = e => {
+      console.log('E', e);
+      serverMessagesList.push(e.data);
+      setServerMessages([...serverMessagesList]);
+    };
+  });
+
+  const submitMessage = () => {
+    ws.send(messageText);
+    setMessageText('');
+    setInputFieldEmpty(true);
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
+    <View style={styles.container}>
+      <View
+        style={{
+          height: 30,
+          backgroundColor: '#eeceff',
+          padding: 5,
+        }}>
+        <Text>{serverState}</Text>
+      </View>
+      <View
+        style={{
+          backgroundColor: '#ffeece',
+          padding: 5,
+          flexGrow: 1,
+        }}>
+        <ScrollView>
+          {serverMessages.map((item, ind) => {
+            return <Text key={ind}>{item}</Text>;
+          })}
+        </ScrollView>
+      </View>
+      <View
+        style={{
+          flexDirection: 'row',
+        }}>
+        <TextInput
           style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+            borderWidth: 1,
+            borderColor: 'black',
+            flexGrow: 1,
+            padding: 5,
+          }}
+          placeholder={'Add Message'}
+          onChangeText={text => {
+            setMessageText(text);
+            setInputFieldEmpty(text.length > 0 ? false : true);
+          }}
+          value={messageText}
+        />
+        <Button
+          onPress={submitMessage}
+          title={'Submit'}
+          disabled={disableButton || inputFieldEmpty}
+        />
+      </View>
+    </View>
   );
 };
-
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  container: {
+    flex: 1,
+    backgroundColor: '#ecf0f1',
+    paddingTop: 30,
+    padding: 8,
   },
 });
 
