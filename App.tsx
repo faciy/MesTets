16b34e99adb2b62e69341aa9fd250bf21ed07765
Yao Kouassi Charles Oth-Niel <charles@192.168.1.77>
2022-10-10 15:41:17 +0000

    scroll

--- App.tsx
+++ App.tsx
@@ -1,114 +1,108 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
-import React from 'react';
+import React, {useState, useRef} from 'react';
 import {
   SafeAreaView,
-  ScrollView,
-  StatusBar,
+  View,
+  FlatList,
   StyleSheet,
   Text,
-  useColorScheme,
-  View,
+  StatusBar,
+  TouchableOpacity,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const DATA = [
+  {
+    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+    title: 'First Item',
+    name: 'charles',
+  },
+  {
+    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+    title: 'Second Item',
+    name: 'kouassi',
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-145571e29d72',
+    title: 'Third Item',
+    name: 'konan',
+  },
+];
 
-const Section: React.FC<{
-  title: string;
-}> = ({children, title}) => {
-  const isDarkMode = useColorScheme() === 'dark';
+const Item = ({title, item, index}) => {
+  const [test, setTest] = useState(false);
+  console.log('ITEM', index);
+
+  const handleAccept = item => {
+    setTest(true);
+  };
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View style={styles.item}>
+      <Text style={styles.title}>{test ? item.name : title}</Text>
+      <TouchableOpacity onPress={() => handleAccept(item)}>
+        <Text style={{backgroundColor: 'white', width: '100%', height: 30}}>
+          ACCEPTER
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 };
-
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+  const [inViewPort, setInViewPort] = useState(1);
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+  const renderItem = ({item, index}) => (
+    <Item item={item} index={index} title={item.title} />
+  );
+
+  const onViewableItemsChanged = useRef(({changed}) => {
+    if (changed && changed.length > 0) {
+      setInViewPort(changed[0].index + 1);
+    }
+  });
+
+  const viewabilityConfig = useRef({
+    itemVisiblePercentThreshold: 50,
+    waitForInteraction: true,
+    minimumViewTime: 5,
+  });
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+    <SafeAreaView style={styles.container}>
+      <View
+        style={{
+          backgroundColor: 'yellow',
+          width: '100%',
+          height: 50,
+        }}>
+        <Text>
+          {' '}
+          {inViewPort} / {DATA.length}{' '}
+        </Text>
+      </View>
+      <FlatList
+        data={DATA}
+        renderItem={(item, index) => renderItem(item, index)}
+        keyExtractor={item => item.id}
+        horizontal={true}
+        onViewableItemsChanged={onViewableItemsChanged.current}
+        viewabilityConfig={viewabilityConfig.current}
+      />
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
+    backgroundColor: 'red',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  item: {
+    backgroundColor: '#f9c2ff',
+    padding: 120,
+    marginVertical: 8,
+    marginHorizontal: 16,
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  title: {
+    fontSize: 32,
   },
 });
 
